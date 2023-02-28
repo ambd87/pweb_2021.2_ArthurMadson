71ea26e927be4d4ea2a0799f4e09377aaeb3187b
Programação WEB - UFERSA Angicos <pwebufersa@gmail.com>
2023-02-28 13:31:44 -0300

    Update PessoaController.java

--- u1_exercicio3/src/main/java/br/com/arthur/cadpessoas/controllers/PessoaController.java
+++ u1_exercicio3/src/main/java/br/com/arthur/cadpessoas/controllers/PessoaController.java
@@ -1,14 +1,18 @@
 package br.com.arthur.cadpessoas.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
 @RequestMapping("/")
 class PessoaController {
 
 	// raiz do site localhost:8080/
 	@GetMapping
-	public String index(){
-		return "index.html"
-		
+	public String index() {
+		return "index.html";
+
 	}
 
 }
