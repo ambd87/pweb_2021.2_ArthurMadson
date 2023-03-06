8326b748053e841c10ca0d6fc2c824afb12d26b2
Programação WEB - UFERSA Angicos <pwebufersa@gmail.com>
2023-03-06 16:40:04 -0300

    Arrumando COntroller

--- u1_exercicio3/src/main/java/br/com/arthur/cadpessoas/controllers/PessoaController.java
+++ u1_exercicio3/src/main/java/br/com/arthur/cadpessoas/controllers/PessoaController.java
@@ -1,58 +1,60 @@
 package br.com.arthur.cadpessoas.controllers;
 
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import br.com.arthur.cadpessoas.model.Pessoa;
 import br.com.arthur.cadpessoas.repositories.PessoaRepository;
 
 @Controller
 @RequestMapping("/")
-public class PessoaController{
-	
-	
-	@AutoWired
+public class PessoaController {
+
+	@Autowired
 	PessoaRepository pessoaRepo;
-	
-	public PessoaController(PessoaRepository pessoaRepo){
-		
+
+	public PessoaController(PessoaRepository pessoaRepo) {
 		this.pessoaRepo = pessoaRepo;
-		
-		
+
 	}
-	
+
 	@GetMapping
-		public String index(){
-			return "index.html";
-			
-		}
-		
+	public String index() {
+		return "index.html";
+
+	}
+
 	@GetMapping("/listarPessoas")
-	public ModelAndView listarPessoas(){
-		
-		List<Pessoa>   todasAsPessoas = pessoaRepo.findAll();
+	public ModelAndView listarPessoas() {
+
+		List<Pessoa> todasAsPessoas = pessoaRepo.findAll();
 		ModelAndView modelAndView = new ModelAndView("listarPessoas");
- 		modelAndView.addObject("todasAsPessoas", todasAsPessoas);
-		
-		return  modelAndView;
-		
+		modelAndView.addObject("todasAsPessoas", todasAsPessoas);
+
+		return modelAndView;
+
 	}
-	 
-	 @GetMapping("/adicionarPessoas")
-	 public ModelAndView formularioAdicionarPessoas(){
-		
-		List<Pessoa>   todasAsPessoas = pessoaRepo.findAll();
+
+	@GetMapping("/adicionarPessoas")
+	public ModelAndView formularioAdicionarPessoas() {
+
+		List<Pessoa> todasAsPessoas = pessoaRepo.findAll();
 		ModelAndView modelAndView = new ModelAndView("adicionarPessoas");
- 		modelAndView.addObject(new Pessoa());
-		
-		return  modelAndView;
-		
+		modelAndView.addObject(new Pessoa());
+		return modelAndView;
+
 	}
-	
-		@PostMapping("/adicionarPessoa")
-			public String adicionarPessoa(Pessoa p){
-				this.pessoaReposave.(p);
-					return "redirect:/listarPessoas";
-			
-			}
+
+	@PostMapping("/adicionarPessoa")
+	public String adicionarPessoa(Pessoa p) {
+		this.pessoaRepo.save(p);
+		return "redirect:/listarPessoas";
+
+	}
 }
