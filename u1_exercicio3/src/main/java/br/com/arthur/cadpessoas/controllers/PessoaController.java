4dc6d409ef0f8e762d15745df42956bfab44f109
ArthurMadsonBatistaDantas <123967424+ArthurMadsonBatistaDantas@users.noreply.github.com>
2023-03-06 17:01:58 -0300

    mudanças

--- u1_exercicio3/src/main/java/br/com/arthur/cadpessoas/controllers/PessoaController.java
+++ u1_exercicio3/src/main/java/br/com/arthur/cadpessoas/controllers/PessoaController.java
@@ -1,51 +1,58 @@
-package br.con.arthur.cadpessoas.controllers:
-import java.util.List;
-Import
-import
-org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller; org.springframework.web.bind.annotation.GetMapping: org.springframework.web.bind.annotation.PostMapping; org.springframework.web.bind.amotation.RequestMapping:
-import Import
-import org.springframework.web.servlet.ModelAndView:
-import br.com.xico.cadpessoas.model.Fesspa;
-import.br.com.xico.cadoessoas.repositories.PessoaRepository;
+package br.com.arthur.cadpessoas.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.arthur.cadpessoas.repositories.PessoaRepository;
+
 @Controller
 @RequestMapping("/")
-
-public PessoaController(Pessoa Repository pessoarepo){ this.pessoaRepo = pessoaRepo;
-
-GetMapping
-public String index()
-}
-return "index.html";
-I
-@GetMapping("/listarPessoas")
-public Model Andview listarPessoas() {
-List<Pessoa> todasAsPessoas pessoa Repo.findALL();
-ModelAndView modelAndView = new ModelAndView("listarPessoas"); nodelAndView.addObject("todasAsPessoas, todasAsPessoas);
-return nodelAndView:
-GetMapping("/adicionarPessoa")
-public ModelAndView formularioAdicionarPessoas() {
-Model Andview model Andview new ModelAndView("adicionarPessoa'); mode Landview.addObject(new Pessoa());
-return nodelAndView;
-
-
-RGetMapping("/listarPessoas")
-public ModelAndview listarPessoas() {
-List<Pessoa> todasAsPessoas pessoaRepo.findALL();
-ModelAndView model Andview new ModelAndView("ListarPessoas"); node LAndView.addObject("todasAsPessoas, todasAsPessoas); return nodelAndView:
-GetMapping("/adicionarPessoa")
-public ModelAndview formularioAdicionarPessoas() |
-}
-Model Andview model Andview = new ModelAndView("adicionarPessoa"); nodeLandView.addObject(new Pessoa());
-return nodelAndView;
-PostMapping/adicionarPessoa
-public String adicionar Pessoa (Pessoa p){
-this.pessoaRepo.save(p);
-return "redirect:/listarPessoas";
-@GetMapping("/remover/{id}")
-public ModelAndView removerPessoa (@PathVariable("id") long id) {
-Pessoa aRemover = pessoaRepo.findById(id)
-.orElseThrow(()->new IllegalArgumentException("ID inválido=" + id));
-pessoaRepo.delete(aRemover);
-return new ModelAndView("redirect:/listarPessoas");
+public class PessoaController{
+	
+	
+	@AutoWired
+	PessoaRepository pessoaRepo;
+	
+	public PessoaController(PessoaRepository pessoaRepo){
+		
+		this.pessoaRepo = pessoaRepo;
+		
+		
+	}
+	
+	@GetMapping
+		public String index(){
+			return "index.html";
+			
+		}
+		
+	@GetMapping("/listarPessoas")
+	public ModelAndView listarPessoas(){
+		
+		List<Pessoa>   todasAsPessoas = pessoaRepo.findAll();
+		ModelAndView modelAndView = new ModelAndView("listarPessoas");
+ 		modelAndView.addObject("todasAsPessoas", todasAsPessoas);
+		
+		return  modelAndView;
+		
+	}
+	 
+	 @GetMapping("/adicionarPessoas")
+	 public ModelAndView formularioAdicionarPessoas(){
+		
+		List<Pessoa>   todasAsPessoas = pessoaRepo.findAll();
+		ModelAndView modelAndView = new ModelAndView("adicionarPessoas");
+ 		modelAndView.addObject(new Pessoa());
+		
+		return  modelAndView;
+		
+	}
+	
+		@PostMapping("/adicionarPessoa")
+			public String adicionarPessoa(Pessoa p){
+				this.pessoaReposave.(p);
+					return "redirect:/listarPessoas";
+			
+			}
 }
--- u1_exercicio3/src/main/java/br/com/arthur/cadpessoas/repositories/PessoaRepository.java
+++ u1_exercicio3/src/main/java/br/com/arthur/cadpessoas/repositories/PessoaRepository.java
@@ -1,5 +1,17 @@
 package br.com.arthur.cadpessoas.repositories;
 
-public class PessoaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-}
+@Repository
+public interface PessoaRepository 
+	
+		extends jpaRepository<Pessoa, Long>{
+			
+			
+			
+		}
+			
+
+
+	
