0fd53d5b6d1823a14b03b67c680628fc9b9972de
ArthurMadsonBatistaDantas <123967424+ArthurMadsonBatistaDantas@users.noreply.github.com>
2023-03-14 15:27:46 -0300

    mudanças
    pessoacontroller.java

--- u1_exercicio3/src/main/java/br/com/arthur/cadpessoas/controllers/PessoaController.java
+++ u1_exercicio3/src/main/java/br/com/arthur/cadpessoas/controllers/PessoaController.java
@@ -3,8 +3,20 @@ package br.com.arthur.cadpessoas.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import br.com.arthur.cadpessoas.repositories.PessoaRepository;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation. PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.vweb. servlet.ModelAndView;
+import br.com.arthur.cadpessoas.model.Pessoas;
+import br.com.arthur.cadpessoas.repositories.PessoaRepository;
+
+eController
+ERequestMapping("/")
+public class Pessoacontroller (
+
 
 @Controller
 @RequestMapping("/")
