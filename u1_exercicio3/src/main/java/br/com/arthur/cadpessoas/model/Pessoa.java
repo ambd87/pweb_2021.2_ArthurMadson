593908962eb403c67224397f419bf3d9137caaf9
Programação WEB - UFERSA Angicos <pwebufersa@gmail.com>
2023-03-14 16:11:26 -0300

    Arrumando controller

--- u1_exercicio3/src/main/java/br/com/arthur/cadpessoas/model/Pessoa.java
+++ u1_exercicio3/src/main/java/br/com/arthur/cadpessoas/model/Pessoa.java
@@ -38,28 +38,3 @@ public class Pessoa implements Serializable {
 	private LocalDate dataNascimento;
 
 }
-public String getNome() (
-return nome;
-}
-public void setNome(String nome) {
-this.nome = nome;
-}
-public String getemail() {
-return email;
-}
-public void setEmail (String email) (
-this.email = email;
-}
-public String getGenero() {
-return genero;
-}
-public void setGenero(String genero) {
-this.genero = genero;
-}
-public LocalDate getDataNascimento() {
-return dataNascimento;
-}
-]
-public void setDataNascimento(LocalDate dataNascimento) {
-this.dataNascimento = dataNascimento;
-}
